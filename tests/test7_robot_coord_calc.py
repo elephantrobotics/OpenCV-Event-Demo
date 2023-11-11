f20745b49a56d3bc3e1687b58d21b1486f97e70f
roggeohta <roggeohta@outlook.com>
2023-11-11 15:07:45 +0800

    add offset support to test7

--- tests/test7_robot_coord_calc.py
+++ tests/test7_robot_coord_calc.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 sys.path.append(".")
 
@@ -8,12 +9,14 @@ from core.config import *
 from core.coord_calc import CoordCalc
 from core.arm_controls import init_arm
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mc = MyCobot("COM6")
     init_arm(mc)
     print("Init complete.")
 
-    coord_calc = CoordCalc(target_base_pos3d, plane_center_pos2d, plane_frame_size_ratio)
+    coord_calc = CoordCalc(
+        target_base_pos3d, plane_center_pos2d, plane_frame_size_ratio
+    )
     test_point = (512 // 2, 512 // 2)
     x, y, z = coord_calc.frame2real(*test_point)
     print(f"Plane center point : {x} {y} {z}")
@@ -22,7 +25,10 @@ if __name__ == '__main__':
     mc.send_angles(arm_pick_hover_angle, 50)
     time.sleep(5)
 
+    x += final_coord_offset[0]
+    y += final_coord_offset[1]
+    z += final_coord_offset[2]
+
     coord = [x, y, z] + perpendicular_angle
     mc.send_coords(coord, 50)
     time.sleep(5)
-
